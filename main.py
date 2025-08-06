33260137c96680137a4a57845aefcfdc5fcaa69a
rodrigofrontoroli-art <rodrigo.frontoroli@gmail.com>
2025-08-06 15:04:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-import os
-import sys
-# DON'T CHANGE THIS !!!
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from flask import Flask, request, jsonify
 
-from flask import Flask, send_from_directory
-from flask_cors import CORS
-from src.models.user import db
-from src.routes.user import user_bp
-from src.routes.webhook import webhook_bp
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
-app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
+@app.route('/')
+def index():
+    return "Leone Brabus está rodando com sucesso!"
 
-# Habilita CORS para todas as rotas
-CORS(app)
-
-app.register_blueprint(user_bp, url_prefix='/api')
-app.register_blueprint(webhook_bp)
-
-# uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-with app.app_context():
-    db.create_all()
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    static_folder_path = app.static_folder
-    if static_folder_path is None:
-            return "Static folder not configured", 404
-
-    if path != "" and os.path.exists(os.path.join(static_folder_path, path)):
-        return send_from_directory(static_folder_path, path)
-    else:
-        index_path = os.path.join(static_folder_path, 'index.html')
-        if os.path.exists(index_path):
-            return send_from_directory(static_folder_path, 'index.html')
-        else:
-            return "index.html not found", 404
-
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Mensagem recebida:", data)
+    return jsonify({"status": "ok"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
