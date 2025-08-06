8c7b7aee3b1e5ae9f2a4d542e3b04e984e4bda3e
rodrigofrontoroli-art <rodrigo.frontoroli@gmail.com>
2025-08-06 15:08:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return "Leone Brabus está rodando com sucesso!"
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
     print("Mensagem recebida:", data)
     return jsonify({"status": "ok"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
