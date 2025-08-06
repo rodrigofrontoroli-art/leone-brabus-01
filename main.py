4f492a953ea6b3df50be0a56d62df211925625a8
rodrigofrontoroli-art <rodrigo.frontoroli@gmail.com>
2025-08-06 14:31:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
-from flask import Flask, request, jsonify
 import os
-import requests
+import sys
+# DON'T CHANGE THIS !!!
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+from src.models.user import db
+from src.routes.user import user_bp
+from src.routes.webhook import webhook_bp
 
-TOKEN = os.getenv("WAZZUP_TOKEN")
-INSTANCE_ID = os.getenv("WAZZUP_INSTANCE_ID")
+app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
+app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 
-@app.route("/")
-def home():
-    return "Leone Brabus está online!"
+# Habilita CORS para todas as rotas
+CORS(app)
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    data = request.json
-    print("Mensagem recebida:", data)
-    try:
-        phone = data["phone"]
-        message = "Olá, aqui é o Leone da Brabus. Como posso te ajudar?"
-        send_message(phone, message)
-    except Exception as e:
-        print("Erro ao responder:", e)
-    return jsonify({"status": "ok"})
+app.register_blueprint(user_bp, url_prefix='/api')
+app.register_blueprint(webhook_bp)
 
-def send_message(phone, text):
-    url = f"https://api.z-api.io/instances/{INSTANCE_ID}/token/{TOKEN}/send-text"
-    payload = {
-        "phone": phone,
-        "message": text
-    }
-    response = requests.post(url, json=payload)
-    print("Resposta da API:", response.text)
+# uncomment if you need to use database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    static_folder_path = app.static_folder
+    if static_folder_path is None:
+            return "Static folder not configured", 404
+
+    if path != "" and os.path.exists(os.path.join(static_folder_path, path)):
+        return send_from_directory(static_folder_path, path)
+    else:
+        index_path = os.path.join(static_folder_path, 'index.html')
+        if os.path.exists(index_path):
+            return send_from_directory(static_folder_path, 'index.html')
+        else:
+            return "index.html not found", 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
