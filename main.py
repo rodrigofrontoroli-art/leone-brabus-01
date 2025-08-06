dd82803f6cd7831a76e9f51016989f05f5870846
rodrigofrontoroli-art <rodrigo.frontoroli@gmail.com>
2025-08-06 12:19:06 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,6 +14,3 @@ def webhook():
     print(f"[{agora}] 📩 Mensagem recebida:")
     print(data)
     return jsonify({"status": "ok"}), 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
